2d10cc22cf7784794d25f606be5248eee803e7cb
PhoebeL27 <plalo27@students.roxbury.org>
2026-01-27 20:48:58 -0500

    making intake system

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,8 +4,23 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.FeedbackSensor;
+
+public class IntakeSubsystem extends SubsystemBase
+{
+    private final SparkFlex sparkFlex;
+    private final RelativeEncoder encoder;
+
+    public IntakeSubsystem()
+    {
+        sparkFlex = new SparkFlex(0, null)
+    }
+}
