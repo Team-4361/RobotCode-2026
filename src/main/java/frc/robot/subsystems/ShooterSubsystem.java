62aeccf7da5dc411434f8c1c2df47dcdb5bd7e10
plalo27 <plalo27@students.roxbury.org>
2026-01-28 20:50:36 -0500

    changes

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,57 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
 public class ShooterSubsystem extends SubsystemBase
 {
+    private final TalonFX shooterKraken;
+    private final SparkFlex indexVortex;
+    private final RelativeEncoder encoder;
     public ShooterSubsystem()
     {
+        shooterKraken = new TalonFX(0); //add to constants  
+        shooterKraken.getConfigurator().apply(new TalonFXConfiguration());
+        var currentLimitsConfigs =   new CurrentLimitsConfigs();
+        currentLimitsConfigs.StatorCurrentLimit = 40; // add value to constants 
+        currentLimitsConfigs.StatorCurrentLimitEnable = true;
+        shooterKraken.getConfigurator().refresh(currentLimitsConfigs);
+        shooterKraken.getConfigurator().apply(currentLimitsConfigs);
+        indexVortex = new SparkFlex(0, MotorType.kBrushless); 
+        SparkFlexConfig config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kBrake);
+        indexVortex.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        encoder = indexVortex.getEncoder();
 
+    } 
+
+    @Override
+    public void periodic()
+    {
+        //SmartDashboard.putNumber()
     }
+
+    public void shooterKraken(double speed)
+    {
+        shooterKraken.set(speed);
+    }
+    public void indexVortex(double speed)
+    {
+        indexVortex.set(speed);
+    }
+
 }
