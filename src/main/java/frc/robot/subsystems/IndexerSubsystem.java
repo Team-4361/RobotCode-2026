ae5370af096f9fa3f7da0e76427fe6c29641b1bf
ConnorNilsen <xdjcat20203416@gmail.com>
2026-03-28 11:53:24 -0400

    Added Index subsystem

---
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.wpilibj2.comma86nd.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class IndexerSubsystem extends SubsystemBase {
+    
+    private final SparkFlex sparkFlex;
+    private final RelativeEncoder encoder;
+    public IndexerSubsystem() {
+        sparkFlex = new SparkFlex(12, MotorType.kBrushless);
+        SparkFlexConfig config = new SparkFlexConfig();
+        sparkFlex.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        config.idleMode(IdleMode.kCoast);
+        sparkFlex.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        encoder = sparkFlex.getEncoder();
+    }
+
+    public void SetMotorSpeed(Double speed) {
+        sparkFlex.set(speed);
+    }
+
+    public void stop() {
+        sparkFlex.stopMotor();
+    }
+
+    // Command to run the motor at a specified speed
+    public Command runMotorCommand(double speed) {
+        return this.runOnce(
+            () -> SetMotorSpeed(speed));
+    }
+
+    // Command to stop the motor
+    public Command stopMotorCommand() {
+        return this.runOnce(() -> stop());
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        // Add any telemetry or monitoring here if needed
+        SmartDashboard.putNumber("Indexer RPM", encoder.getVelocity());
+    }
+}
+
